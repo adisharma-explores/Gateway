6fdda18a4102b75735952b467de8d35b9b7992c8
adisharma-explores <adisharma191504@gmail.com>
2024-11-29 11:04:18 +0530

    src

---
+++ src/main/java/com/ecommerce/api_gateway/filter/JwtAthFilter.java
@@ -0,0 +1,69 @@
+package com.ecommerce.api_gateway.filter;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Consumer;
+
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+
+@Component
+public class JwtAthFilter implements GlobalFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public JwtAthFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+    private Mono<Void> unauthorized(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        DataBuffer buffer = exchange.getResponse().bufferFactory().wrap("Unauthorized".getBytes(StandardCharsets.UTF_8));
+        return exchange.getResponse().writeWith(Mono.just(buffer));
+    }
+    private Mono<Void> expired(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        DataBuffer buffer = exchange.getResponse().bufferFactory().wrap("Token Expired".getBytes(StandardCharsets.UTF_8));
+        return exchange.getResponse().writeWith(Mono.just(buffer));
+    }
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return unauthorized(exchange);
+        }
+
+        String token = authHeader.substring(7);
+
+        try {
+            // Validate the token
+            if(jwtUtil.isTokenExpired(token)){
+                return expired(exchange);
+            }
+            ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+                    .header("X-User", jwtUtil.extractUsername(token))
+                    .header("X-Roles", jwtUtil.getClaim(token,"roles").toString())
+                    .header("X-UserID", jwtUtil.getClaim(token,"userId").toString())
+                    .build();
+            // Add claims to headers for downstream services
+            mutatedRequest.getHeaders().forEach((key, value) -> {
+                System.out.println(key + ": " + value);
+            });
+            return chain.filter(exchange.mutate().request(mutatedRequest).build());
+        } catch (Exception e) {
+            return unauthorized(exchange);
+        }
+    }
+}
---
+++ src/main/java/com/ecommerce/api_gateway/filter/JwtUtil.java
@@ -0,0 +1,55 @@
+package com.ecommerce.api_gateway.filter;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Claims;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+    public JwtUtil() {
+    }
+
+    private static final String AUTHOTRIZATION = "@dityaE-commerce-$*!@!^@)@$@*#@#^$)";
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public Object getClaim(String token, String claimName) {
+        final Claims claims = extractAllClaims(token);
+        return claims.get(claimName);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    Claims extractAllClaims(String token) {
+        SecretKey key=  Keys.hmacShaKeyFor(AUTHOTRIZATION.getBytes());
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+}
